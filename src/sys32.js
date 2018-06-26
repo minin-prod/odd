d0dd05ae3b5629b20a4b6b66b286e8a1b322dd77
MininMobile <notminin@gmail.com>
2018-06-26 23:18:27 +0200

    add navbar

--- src/sys32.js
+++ src/sys32.js
@@ -1,5 +1,12 @@
 new Promise((result, reject) => {
-	result(true);
+	let container = document.getElementById("odd-container");
+
+	{
+		let navbar = document.createElement("nav");
+		container.appendChild(navbar);
+	}
+
+	result(container);
 }).then((res, err) => {
 	document.getElementById("preloader").remove();
 });
