e1e12997f7d15724329d13261fb87c0f1711f4a1
MininMobile <notminin@gmail.com>
2018-06-27 14:28:43 +0200

    better coloring

--- src/dll/appearance.js
+++ src/dll/appearance.js
@@ -1,27 +1,33 @@
-var settings = {};
-
 dll.appearance = {};
 
-dll.appearance.settings = settings = {
-	"backgroundStyle": "solid",
-	"background": "#1c1c1c",
-	"accent": "0, 0, 0",
-	"textColor": "#fff"
+dll.appearance.settings = {
+	"wallpaper": "#1c1c1c",
+	"accentRGBAS": "0, 0, 0",
+	"accent": "#000",
+	"textColor": "#ddd"
 }
 
 dll.appearance.init = () => {
+	let settings = dll.appearance.settings;
+
+	let head = document.getElementsByTagName("head")[0];
 	let layerWall = document.getElementsByClassName("wall")[0];
 	let layerNav = document.getElementsByClassName("nav")[0];
 
 	{
-		if (settings.backgroundStyle == "solid") {
-			layerWall.style.backgroundColor = settings.background;
-		}
+		let _settings = ":root {\n";
+		Object.keys(settings).forEach((set) => {
+			_settings += `\t--${set}: ${settings[set]};\n`
+		});
+		_settings += "}";
+
+		let _style = document.createElement("style");
+		_style.innerHTML = _settings;
+		head.appendChild(_style);
 	}
 
 	{
 		let navbar = layerNav.getElementsByTagName("nav")[0];
-		navbar.style.backgroundColor = `rgba(${settings.accent}, 0.2)`;
-		navbar.style.color = settings.textColor;
+		navbar.style.backgroundColor = `rgba(${settings.accentRGBAS}, 0.2)`;
 	}
 }
