bdb2170b7827e8fdb4dae8de6fa8c1a09d2aa7ee
MininMobile <notminin@gmail.com>
2018-06-27 12:14:18 +0200

    style navbar

--- src/dll/appearance.js
+++ src/dll/appearance.js
@@ -4,15 +4,22 @@ dll.appearance = {};
 
 dll.appearance.settings = settings = {
 	"backgroundStyle": "solid",
-	"background": "#1c1c1c"
+	"background": "#1c1c1c",
+	"accent": "0, 0, 0"
 }
 
 dll.appearance.init = () => {
 	let layerWall = document.getElementsByClassName("wall")[0];
+	let layerNav = document.getElementsByClassName("nav")[0];
 
 	{
 		if (settings.backgroundStyle == "solid") {
 			layerWall.style.backgroundColor = settings.background;
 		}
+	}
+
+	{
+		let navbar = layerNav.getElementsByTagName("nav")[0];
+		navbar.style.backgroundColor = `rgba(${settings.accent}, 0.2)`;
 	}
 }
