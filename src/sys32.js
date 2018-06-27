e4c3d1c25e843c2ac9f6cd4e97831712dcf9697e
MininMobile <notminin@gmail.com>
2018-06-27 11:21:56 +0200

    add layers

--- src/sys32.js
+++ src/sys32.js
@@ -1,9 +1,12 @@
 new Promise((result, reject) => {
 	let container = document.getElementById("odd-container");
+	let layerWall = document.createElement("div"); layerWall.classList.add("layer", "wall"); container.appendChild(layerWall);
+	let layerApp = document.createElement("div"); layerApp.classList.add("layer", "app"); container.appendChild(layerApp);
+	let layerNav = document.createElement("div"); layerNav.classList.add("layer", "nav"); container.appendChild(layerNav);
 
 	{
 		let navbar = document.createElement("nav");
-		container.appendChild(navbar);
+		layerNav.appendChild(navbar);
 	}
 
 	result(container);
