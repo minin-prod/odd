e961f5ddb5ee7da2d8033dce4c25e1aa90f07358
MininMobile <notminin@gmail.com>
2018-06-29 15:24:39 +0200

    add cd methods to manager

--- src/dll/hdd.js
+++ src/dll/hdd.js
@@ -27,3 +27,32 @@ dll.hdd.util = {
 	"isRoot": (loc) => { if (loc.length == 1 && loc[0] == "root") { return true } else { return false } },
 	"isEmpty": (loc) => { if (loc.length == 0) { return true } ekse { return false } }
 }
+
+dll.hdd.manager = class Manager {
+	constructor() {
+		this.loc = ["root"];
+		this.current = dll.hdd.fs["root"];
+	}
+
+	cd(dir) {
+		if (dir == "..") {
+			if (!dll.hdd.util.isEmpty(this.loc.pop()))
+				this.loc.pop();
+		} else {
+			this.loc.push(dir);
+		}
+	}
+
+	cdlist(navto) {
+		this.loc = [];
+
+		navto.forEach((dir) => {
+			if (dir == "..") {
+				if (!dll.hdd.util.isEmpty(this.loc.pop()))
+					this.loc.pop();
+			} else {
+				this.loc.push(dir);
+			}
+		});
+	}
+}
