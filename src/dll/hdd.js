bfbb1c532e93c1e0da755a1bcb0db0d7990543e6
MininMobile <notminin@gmail.com>
2018-06-29 15:23:46 +0200

    add file system

---
+++ src/dll/hdd.js
@@ -0,0 +1,24 @@
+dll.hdd = {};
+
+dll.hdd.fs = {
+	"root": {
+		"root": {
+			type: "dir"
+		},
+		"log": {
+			type: "dir"
+		},
+		"bin": {
+			type: "dir"
+		},
+		"lib": {
+			type: "dir"
+		},
+		"usr": {
+			type: "dir"
+		},
+		"home": {
+			type: "dir"
+		}
+	}
+}
