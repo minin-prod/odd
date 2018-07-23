2d79867234491c6824550e7dfe3aa7633f6a2bf9
MininMobile <notminin@gmail.com>
2018-07-23 19:38:31 +0200

    add time style

--- src/dll/taskbar.js
+++ src/dll/taskbar.js
@@ -7,13 +7,14 @@ dll.taskbar.init = () => {
 
 	{
 		let dt = document.createElement("div");
+		dt.classList.add("time")
 
 		sectionRight.appendChild(dt);
 
 		dll.taskbar.timeService = setInterval(() => {
 			let t = new Date();
 
-			dt.innerText = `${t.getHours()}:${t.getMinutes()}`;
+			dt.innerText = `${t.getHours()}:${t.getMinutes()}\n${t.getMonth()+1}/${t.getDate()}/${t.getFullYear()}`;
 		}, 1000);
 	}
 }
