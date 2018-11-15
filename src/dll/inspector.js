7c331e5d03622a2971b611e28ea5ba844316dcad
MininMobile <notminin@pm.me>
2018-11-15 23:02:33 +0000

    add tabs

--- src/dll/inspector.js
+++ src/dll/inspector.js
@@ -3,8 +3,14 @@ dll.inspector = {};
 /** @type {HTMLElement} */
 dll.inspector.inspector = undefined;
 
+/** @type {HTMLElement} */
+dll.inspector.tabs = undefined;
+/** @type {HTMLElement} */
+dll.inspector.content = undefined;
+
 dll.inspector.open = false;
-dll.inspector.width = 500;
+dll.inspector.width = 400;
+dll.inspector.panelHeight = 400;
 
 dll.inspector.toggle = () => {
 	let inspector = dll.inspector.inspector;
@@ -13,8 +19,35 @@ dll.inspector.toggle = () => {
 	dll.inspector.open = !inspector.classList.contains("minimized");
 }
 
+dll.inspector.selectTab = (tab) => {
+	switch (tab) {
+		case "inspector": {
+			// code
+		} break;
+
+		case "styles": {
+			// code
+		} break;
+
+		case "console": {
+			// code
+		} break;
+
+		default: {
+			throw new Error(`Invalid Tab: ${tab}
+Expected Values:
+	- "inspector"
+	- "styles"
+	- "console"`);
+		}
+	}
+}
+
 dll.inspector.init = async () => { return new Promise((res, err) => {
 	let inspector;
+
+	let tabs;
+	let content;
 
 	inspector = dll.inspector.inspector = document.getElementById("odd-inspector");
 
@@ -23,11 +56,29 @@ dll.inspector.init = async () => { return new Promise((res, err) => {
 
 	{ // inspector content
 		inspector.innerHTML = `
-			<div id="resize-handle" class="resize-handle"></div>
+			<div class="devtools-resize-handle"></div>
 			<div class="container">
-				<!-- content -->
+				<div class="tablist"></div>
+				<div class="wrapper"></div>
 			</div>
 		`;
+	}
+
+	tabs = dll.inspector.tabs = inspector.children[1].children[0];
+	content = dll.inspector.content = inspector.children[1].children[1];
+
+	{ // tab list
+		let tabInspector = document.createElement("div"); tabs.appendChild(tabInspector);
+		let tabInspectorText = document.createElement("div"); tabInspector.appendChild(tabInspectorText);
+		tabInspectorText.innerText = "Inspector"; tabInspector.classList.add("active");
+
+		let tabStyles = document.createElement("div"); tabs.appendChild(tabStyles);
+		let tabStylesText = document.createElement("div"); tabStyles.appendChild(tabStylesText);
+		tabStylesText.innerText = "Styles";
+
+		let tabConsole = document.createElement("div"); tabs.appendChild(tabConsole);
+		let tabConsoleText = document.createElement("div"); tabConsole.appendChild(tabConsoleText);
+		tabConsoleText.innerText = "Console";
 	}
 
 	{ // shortcuts
